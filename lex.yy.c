50849bb807f85f7a48430c6540bdd858bd91f99c
Achyut-shekhar <achyutshekhar54@gmail.com>
2026-02-09 09:37:13 +0530

    Add DFA implementations for binary strings with specific conditions and refactor existing files

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
+#define YY_NUM_RULES 13
+#define YY_END_OF_BUFFER 14
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,19 +360,19 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[14] =
+static const flex_int16_t yy_accept[21] =
     {   0,
-        0,    0,    6,    4,    3,    3,    2,    2,    3,    2,
-        2,    1,    0
+        0,    0,    0,    0,    0,    0,   14,    3,    4,    2,
+        1,    7,    8,    6,    5,   11,   12,    9,   10,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
-        1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    4,    5,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    3,    4,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,33 +398,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[6] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    2,    2,    1,    1
+        1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[15] =
+static const flex_int16_t yy_base[21] =
     {   0,
-        0,    0,   10,   11,    0,    0,    2,    5,    0,    0,
-        0,    0,   11,    6
+        0,    0,    4,    0,    8,    0,   13,   14,   14,   14,
+       14,   14,   14,   14,   14,   14,   14,   14,   14,   14
     } ;
 
-static const flex_int16_t yy_def[15] =
+static const flex_int16_t yy_def[21] =
     {   0,
-       13,    1,   13,   13,   14,   14,   13,    7,   14,    7,
-        8,    7,    0,   13
+       20,    1,   20,    3,   20,    5,   20,   20,   20,   20,
+       20,   20,   20,   20,   20,   20,   20,   20,   20,    0
     } ;
 
-static const flex_int16_t yy_nxt[17] =
+static const flex_int16_t yy_nxt[19] =
     {   0,
-        4,    5,    6,    7,    8,   10,   11,    9,   12,   13,
-        3,   13,   13,   13,   13,   13
+        8,    9,   10,   11,   12,   13,   14,   15,   16,   17,
+       18,   19,   20,    7,   20,   20,   20,   20
     } ;
 
-static const flex_int16_t yy_chk[17] =
+static const flex_int16_t yy_chk[19] =
     {   0,
-        1,    1,    1,    1,    1,    7,    7,   14,    8,    3,
-       13,   13,   13,   13,   13,   13
+        1,    1,    1,    1,    3,    3,    3,    3,    5,    5,
+        5,    5,    7,   20,   20,   20,   20,   20
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -441,15 +441,17 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "dfa_lex.l"
-#line 2 "dfa_lex.l"
-#include <stdio.h>
-extern FILE *yyin;
+#line 1 "dfa_even.l"
+#line 2 "dfa_even.l"
+  #include<stdio.h>
+  #include<string.h>
 #line 449 "lex.yy.c"
-/* Pattern that matches strings ending with "10" */
+
 #line 451 "lex.yy.c"
 
 #define INITIAL 0
+#define A 1
+#define B 2
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -664,10 +666,9 @@ YY_DECL
 		}
 
 	{
-#line 7 "dfa_lex.l"
+#line 7 "dfa_even.l"
 
-
-#line 671 "lex.yy.c"
+#line 672 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -694,13 +695,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 14 )
+				if ( yy_current_state >= 21 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 11 );
+		while ( yy_base[yy_current_state] != 14 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -726,32 +727,76 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 9 "dfa_lex.l"
-{ printf("ACCEPTED: %s (ends with 10)\n", yytext); }
+#line 8 "dfa_even.l"
+BEGIN INITIAL;
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 11 "dfa_lex.l"
-{ printf("REJECTED: %s (does not end with 10)\n", yytext); }
+#line 9 "dfa_even.l"
+BEGIN A;
 	YY_BREAK
 case 3:
-/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 13 "dfa_lex.l"
-;  /* Skip whitespace */
+#line 10 "dfa_even.l"
+BEGIN B;
 	YY_BREAK
 case 4:
+/* rule 4 can match eol */
 YY_RULE_SETUP
-#line 15 "dfa_lex.l"
-{ printf(" ERROR: Invalid character %s\n", yytext); }
+#line 11 "dfa_even.l"
+{BEGIN INITIAL; printf("Accepted\n");}
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 17 "dfa_lex.l"
+#line 12 "dfa_even.l"
+BEGIN A;
+	YY_BREAK
+case 6:
+YY_RULE_SETUP
+#line 13 "dfa_even.l"
+BEGIN INITIAL;
+	YY_BREAK
+case 7:
+YY_RULE_SETUP
+#line 14 "dfa_even.l"
+BEGIN B;
+	YY_BREAK
+case 8:
+/* rule 8 can match eol */
+YY_RULE_SETUP
+#line 15 "dfa_even.l"
+{BEGIN INITIAL; printf("NOT Accepted\n");}
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 16 "dfa_even.l"
+BEGIN B;
+	YY_BREAK
+case 10:
+YY_RULE_SETUP
+#line 17 "dfa_even.l"
+BEGIN B;
+	YY_BREAK
+case 11:
+YY_RULE_SETUP
+#line 18 "dfa_even.l"
+BEGIN B;
+	YY_BREAK
+case 12:
+/* rule 12 can match eol */
+YY_RULE_SETUP
+#line 19 "dfa_even.l"
+{BEGIN INITIAL; printf("NOT Accepted\n");}
+	YY_BREAK
+case 13:
+YY_RULE_SETUP
+#line 20 "dfa_even.l"
 ECHO;
 	YY_BREAK
-#line 754 "lex.yy.c"
+#line 797 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
+case YY_STATE_EOF(A):
+case YY_STATE_EOF(B):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1047,7 +1092,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 14 )
+			if ( yy_current_state >= 21 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1075,11 +1120,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 14 )
+		if ( yy_current_state >= 21 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 13);
+	yy_is_jam = (yy_current_state == 20);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1755,26 +1800,18 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 17 "dfa_lex.l"
+#line 20 "dfa_even.l"
 
 
-int main() {
-    yyin = fopen("dfa_test.txt", "r");
-    if (!yyin) {
-        printf("Error: Could not open input.txt\n");
-        return 1;
-    }
-
-    printf("DFA in Lex - Accepts binary strings ending with '10'\n");
-    printf("Reading input from input.txt...\n\n");
-
-    yylex();
-
-    fclose(yyin);
-    return 0;
+int yywrap(){
+  return 1;
 }
 
-int yywrap() {
-    return 1;
+int main(){
+  printf("DFA - Accepts strings with even number of 0s\n");
+  printf("Enter binary strings (press Ctrl+D or Ctrl+Z to exit):\n\n");
+  
+  yylex();
+  
+  return 0;
 }
-
